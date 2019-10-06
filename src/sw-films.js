69289b99067bc27a7abb5309503399626308984f
Ismael Bernal <ismaelbernal83@gmail.com>
2019-10-06 12:32:40 +0200

    Interacción con el DOM sencilla. Eventos y props

--- src/sw-films.js
+++ src/sw-films.js
@@ -22,8 +22,8 @@ class SwFilms extends PolymerElement {
         }
       </style>
 
-      <div class="card">
-        <div class="circle">1</div>
+      <div on-click='changeNameEventListener' class="card">
+        <div id="nombre" class="circle">[[nombre]]</div>
         <h1>View One</h1>
         <p>Ut labores minimum atomorum pro. Laudem tibique ut has.</p>
         <p>Lorem ipsum dolor sit amet, per in nusquam nominavi periculis, sit elit oportere ea.Lorem ipsum dolor sit amet, per in nusquam nominavi periculis, sit elit oportere ea.Cu mei vide viris gloriatur, at populo eripuit sit.</p>
@@ -32,31 +32,67 @@ class SwFilms extends PolymerElement {
   }
 
   /**
-   * Flujo de vida de un webcomponent
+   * Return de properties
+   */
+  static get properties() {
+    return {
+      nombre: {
+        type: String, // String|Number|Boolean|Array|Object|Date
+        value: 'Isma',
+        // reflectToAttribute: true
+      }
+    }
+  }
+
+  /**
+   * Web component constructor
    */
   constructor() {
     super();
-    console.log('constructor');
   }
+  
+  /**
+   * Web component conectado al DOM
+   */
   connectedCallback() {
     super.connectedCallback()
-    console.log('connectedCallback');
+    console.log('##########################');
+    console.log('## ACCESO AL DOM DEL WC ##');
+    // Acceso directo con el $. Sólo sirve para buscar elementos en el shadow DOM por ID
+    console.log(this.$.number);
+    // Esto no va a funcionar. el web component está en el shadow DOM, no en el DOM
+    console.log(document.querySelector('.card')); 
+    // Acceso a través del shadow DOM con funciones nativas del DOM (como query selector)
+    console.log(this.shadowRoot.querySelector('.card'));
+    console.log('##########################');
   }
 
+  /**
+   * Web component ready
+   */
+  ready () {
+    super.ready()
+  }
+
+  /**
+   * Estos dos no los utilizaremos
   disconnectedCallback() {
     super.disconnectedCallback()
     console.log('disconnectedCallback');
-  }
-
-  ready () {
-    super.ready()
-    console.log('ready');
   }
 
   attributeChangedCallback () {
     super.attributeChangedCallback()
     console.log('attributeChangedCallback');
   }
+   */
+
+   /**
+    * Interactuando con el shadow DOM
+    */
+   changeNameEventListener () {
+     this.nombre = this.nombre === 'Isma' ? 'Tam' : 'Isma';
+   }
 }
 
 window.customElements.define('sw-films', SwFilms);
